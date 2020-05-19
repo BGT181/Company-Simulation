77b6cd34be6c52a014b6f13a692cb7810e211347
corneliusdinklage <corneliusdinklage@corneliuss-mbp.multi.box>
2020-05-19 18:26:19 +0200

    No Comment

--- Company Simulation/src/com/gamelogic/ImageProvider.java
+++ Company Simulation/src/com/gamelogic/ImageProvider.java
@@ -9,20 +9,20 @@ public class ImageProvider {
 	private Image[] imageBuffer = new Image[15];
 	
 	public enum Imagefor {
-		BUILDING,	//#0
-		MACHINE_A, 	//#1
-		MACHINE_B,	//#2
-		MACHINE_C,	//#3
-		STORAGE, 	//#4
-		TRUCK,		//#5
-		EMPLOYEE_A,	//#6
-		EMPLOYEE_B,	//#7
-		EMPLOYEE_C,	//#8
-		PRODUCT_A, 	//#9
-		PRODUCT_A_PROCESSED, //#10
-		PRODUCT_B,  //#11
-		PRODUCT_B_PROCESSED, //#12
-		PRODUCT_C,	//#13
+		BUILDING,			//#0
+		MACHINE_A, 			//#1
+		MACHINE_B,			//#2
+		MACHINE_C,			//#3
+		STORAGE, 			//#4
+		TRUCK,				//#5
+		EMPLOYEE_A,			//#6
+		EMPLOYEE_B,			//#7
+		EMPLOYEE_C,			//#8
+		PRODUCT_A, 			//#9
+		PRODUCT_A_PROCESSED,//#10
+		PRODUCT_B,  		//#11
+		PRODUCT_B_PROCESSED,//#12
+		PRODUCT_C,			//#13
 		PRODUCT_C_CERTIFIED //#14
 	}
 	
@@ -32,11 +32,7 @@ public class ImageProvider {
 	
 	private void loadImages() {
 		imageBuffer[0] = new ImageIcon("src/images/building.png").getImage();
-		imageBuffer[5] = new ImageIcon("src/images/LKW.png").getImage();
-		/*
-		Image image0 = new ImageIcon("src/images/building.png").getImage();
-		Image image1 = new ImageIcon("src/images/building.png").getImage();#
-		*/
+		imageBuffer[5] = new ImageIcon("src/images/truck.png").getImage();
 	}
 	
 	public Image getImage(Imagefor image) {
